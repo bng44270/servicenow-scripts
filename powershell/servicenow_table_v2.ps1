323f92bb91d7eb787be361d097111cb793922d4a
Andy Carlson <bng44270@gmail.com>
2026-05-21 13:13:37 -0500

    Update servicenow_table_v2.ps1

--- powershell/servicenow_table_v2.ps1
+++ powershell/servicenow_table_v2.ps1
@@ -29,6 +29,10 @@
 #  3. Run the one of the operation functions:
 #
 #        $resp = (Invoke-ServiceNowQuery -Connection $conn -Table "incident" -Query $queryBuilder)
+#
+#            OR (Invoke-ServiceNowQuery can you -Query or -SysId - NOT BOTH)
+#
+#        $resp = (Invoke-ServiceNowQuery -Connection $conn -Table "incident" -SysId "1bc4415dcfc634c5e9e45b053273f3e0")
 #        
 #        $resp = (Invoke-ServiceNowInsert -Connection $conn -Table "incident" -Data $data)
 #        
@@ -53,230 +57,236 @@
 ####################
 
 function New-ServiceNowConnection() {
-    return [pscustomobject]@{
-        "creds" = (Get-Credential -Title "New ServiceNow Connection")
-        "host"  = (Read-Host -Prompt "Host")
-    }
+  return [pscustomobject]@{
+    "creds" = (Get-Credential -Title "New ServiceNow Connection")
+    "host"  = (Read-Host -Prompt "Host")
+  }
 }
   
 function New-ServiceNowData() {
-    $ob = [pscustomobject]@{
-        "data" = [pscustomobject]@{}
-    }
+  $ob = [pscustomobject]@{
+    "data" = [pscustomobject]@{}
+  }
     
-    $ob | Add-Member -MemberType ScriptMethod -Name "SetValue" -Value {
-        param($fp, $vp)
-        $Exists = (Get-Member -InputObject $this.GetData() -Name $fp)
+  $ob | Add-Member -MemberType ScriptMethod -Name "SetValue" -Value {
+    param($fp, $vp)
+    $Exists = (Get-Member -InputObject $this.GetData() -Name $fp)
       
-        if ($Exists -eq $Null) {
-            $this.data | Add-Member -MemberType NoteProperty -Name $fp -Value $vp    
-        }
-        else {
-            $this.data.$fp = $vp
-        }
+    if ($Null -eq $Exists) {
+      $this.data | Add-Member -MemberType NoteProperty -Name $fp -Value $vp    
+    }
+    else {
+      $this.data.$fp = $vp
+    }
       
-    }
+  }
     
-    $ob | Add-Member -MemberType ScriptMethod -Name "GetData" -Value {
-        return $this.data
-    }
+  $ob | Add-Member -MemberType ScriptMethod -Name "GetData" -Value {
+    return $this.data
+  }
     
-    return $ob
+  return $ob
 }
   
 function Invoke-ServiceNowInsert($Connection, $Table, $Data) {
-    # $Connection should be an instance of New-ServiceNowConnection
-    # $Data should be an instance of New-ServiceNowData
-    $sncred = $Connection.creds
-    $snhost = $Connection.host
+  # $Connection should be an instance of New-ServiceNowConnection
+  # $Data should be an instance of New-ServiceNowData
+  $sncred = $Connection.creds
+  $snhost = $Connection.host
     
-    $postJson = ($Data.GetData() | ConvertTo-Json)
+  $postJson = ($Data.GetData() | ConvertTo-Json)
   
-    $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/json" } -Body $postJson -Method POST -Credential $sncred "https://$snhost/api/now/table/$Table")
+  $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/json" } -Body $postJson -Method POST -Credential $sncred "https://$snhost/api/now/table/$Table")
   
-    $returnValue = $False
+  $returnValue = $False
   
-    if (($resp.StatusCode - 200) -lt 100) {
-        $returnValue = ($resp.Content | ConvertFrom-Json).result.sys_id
-    }
+  if (($resp.StatusCode - 200) -lt 100) {
+    $returnValue = ($resp.Content | ConvertFrom-Json).result.sys_id
+  }
     
-    return $returnValue
+  return $returnValue
 }
   
 function Invoke-ServiceNowUpdate($Connection, $Table, $SysId, $Data) {
-    $sncred = $Connection.creds
-    $snhost = $Connection.host
+  $sncred = $Connection.creds
+  $snhost = $Connection.host
       
-    $postJson = ($Data.GetData() | ConvertTo-Json)
-    $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/json" } -Body $postJson -Method PUT -Credential $sncred "https://$snhost/api/now/table/$Table/$SysId")
+  $postJson = ($Data.GetData() | ConvertTo-Json)
+  $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/json" } -Body $postJson -Method PUT -Credential $sncred "https://$snhost/api/now/table/$Table/$SysId")
       
-    return ($resp.StatusCode - 200) -lt 100
+  return ($resp.StatusCode - 200) -lt 100
 }
 
 function Invoke-ServiceNowAttach($Connection, $Table, $SysId, $FilePath) {
-    $sncred = $Connection.creds
-    $snhost = $Connection.host
+  $sncred = $Connection.creds
+  $snhost = $Connection.host
       
-    $UploadData = [System.IO.File]::ReadAllBytes($FilePath)
-    $FileName = (Get-Item $FilePath).Name
+  $UploadData = [System.IO.File]::ReadAllBytes($FilePath)
+  $FileName = (Get-Item $FilePath).Name
 
-    $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/octet-stream" } -Body $UploadData -Method POST -Credential $sncred "https://$snhost/api/now/attachment/file?table_name=$Table&table_sys_id=$SysId&file_name=$FileName")
+  $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/octet-stream" } -Body $UploadData -Method POST -Credential $sncred "https://$snhost/api/now/attachment/file?table_name=$Table&table_sys_id=$SysId&file_name=$FileName")
       
-    return ($resp.StatusCode - 200) -lt 100
+  return ($resp.StatusCode - 200) -lt 100
 }
 
-function New-ServiceNowQueryBuilder($IsOr=$False) {
-    $ob = [pscustomobject]@{
-      "isor" = $IsOr
-      "qar" = @()
-      "query" = ""
-    }
-    
-    $ob | Add-Member -MemberType ScriptMethod -Name "Add" -Value {
-      param($q)
-      
-      $this.qar += $q
-    }
-    
-    $ob | Add-Member -MemberType ScriptMethod -Name "GetQuery" -Value {
-      $returnValue = $null
-      
-      if ($this.isor) {
-        $returnValue = ($this.qar -join "^OR")
-      }
-      else {
-        $returnValue = ($this.qar -join "^")
-      }
-      
-      return $returnValue
-    }
-    
-    return $ob
+function New-ServiceNowQueryBuilder($IsOr = $False) {
+  $ob = [pscustomobject]@{
+    "isor"  = $IsOr
+    "qar"   = @()
+    "query" = ""
   }
-  
-  function New-ServiceNowQuery($FieldName) {
-    $ob = [pscustomobject]@{
-      "name" = $FieldName
-    }
     
-    $ob | Add-Member -MemberType ScriptMethod -Name "Contains" {
-      param($v)
+  $ob | Add-Member -MemberType ScriptMethod -Name "Add" -Value {
+    param($q)
       
-      return ($this.name + "CONTAINS" + $v)
-    }
-      
-    $ob | Add-Member -MemberType ScriptMethod -Name "Is" {
-      param($v)
-      
-      return ($this.name + "=" + $v)
-    }
-
-    $ob | Add-Member -MemberType ScriptMethod -Name "IsNot" {
-      param($v)
-      
-      return ($this.name + "!=" + $v)
-    }
-
-    $ob | Add-Member -MemberType ScriptMethod -Name "IsEmpty" {
-      return ($this.name + "ISEMPTY")
-    }
-
-    $ob | Add-Member -MemberType ScriptMethod -Name "IsNotEmpty" {
-      return ($this.name + "ISNOTEMPTY")
-    }
-      
-    $ob | Add-Member -MemberType ScriptMethod -Name "StartsWith" {
-      param($v)
-      
-      return ($this.name + "STARTSWITH" + $v)
-    }
-      
-    $ob | Add-Member -MemberType ScriptMethod -Name "EndsWith" {
-      param($v)
-      
-      return ($this.name + "ENDSWITH" + $v)
-    }
-
-    $ob | Add-Member -MemberType ScriptMethod -Name "In" {
-      param($a)
-
-      return ($this.name + "IN" + ($a -join ','))
-    }
-      
-    return $ob 
+    $this.qar += $q
   }
+    
+  $ob | Add-Member -MemberType ScriptMethod -Name "GetQuery" -Value {
+    $returnValue = $null
+      
+    if ($this.isor) {
+      $returnValue = ($this.qar -join "^OR")
+    }
+    else {
+      $returnValue = ($this.qar -join "^")
+    }
+      
+    return $returnValue
+  }
+    
+  return $ob
+}
   
-  function Invoke-ServiceNowQuery($Connection,$Table,$Query,$Limit=10000,$Offset=0,$Fields=@()) {
-    # Query param must be an instance of New-ServiceNowQueryBuilder
-    # Specifiy custom Limit param to set result limit 
-    $returnValue = $False
+function New-ServiceNowQuery($FieldName) {
+  $ob = [pscustomobject]@{
+    "name" = $FieldName
+  }
+    
+  $ob | Add-Member -MemberType ScriptMethod -Name "Contains" {
+    param($v)
+      
+    return ($this.name + "CONTAINS" + $v)
+  }
+      
+  $ob | Add-Member -MemberType ScriptMethod -Name "Is" {
+    param($v)
+      
+    return ($this.name + "=" + $v)
+  }
 
-    $sncred = $Connection.creds
-    $snhost = $Connection.host
+  $ob | Add-Member -MemberType ScriptMethod -Name "IsNot" {
+    param($v)
+      
+    return ($this.name + "!=" + $v)
+  }
+
+  $ob | Add-Member -MemberType ScriptMethod -Name "IsEmpty" {
+    return ($this.name + "ISEMPTY")
+  }
+
+  $ob | Add-Member -MemberType ScriptMethod -Name "IsNotEmpty" {
+    return ($this.name + "ISNOTEMPTY")
+  }
+      
+  $ob | Add-Member -MemberType ScriptMethod -Name "StartsWith" {
+    param($v)
+      
+    return ($this.name + "STARTSWITH" + $v)
+  }
+      
+  $ob | Add-Member -MemberType ScriptMethod -Name "EndsWith" {
+    param($v)
+      
+    return ($this.name + "ENDSWITH" + $v)
+  }
+
+  $ob | Add-Member -MemberType ScriptMethod -Name "In" {
+    param($a)
+
+    return ($this.name + "IN" + ($a -join ','))
+  }
+      
+  return $ob 
+}
+  
+function Invoke-ServiceNowQuery($Connection, $Table, $Query = (New-ServiceNowQueryBuilder), $SysId = "", $Limit = 0, $Offset = 0, $Fields = @()) {
+  # Query param must be an instance of New-ServiceNowQueryBuilder
+  $returnValue = $False
+
+  $sncred = $Connection.creds
+  $snhost = $Connection.host
     
-    $QueryText = $Query.GetQuery()
+  $ReqUrl = "$Table";
+
+  if ($SysId.Length -gt 0) {
+    $ReqUrl += "/$SysId"
+  }
+  else {
+    $ReqUrl += ("`?sysparm_query=" + $Query.GetQuery())
     
-    $OffsetString = ""
+    if ($Limit -gt 0) {
+      $ReqUrl += ("&sysparm_limit=" + $Limit.ToString())
+    }
 
     if ($Offset -gt 0) {
-      $OffsetString = ("&sysparm_offset=" + $Offset)
+      $ReqUrl +=  ("&sysparm_offset=" + $Offset.ToString())
     }
 
-    $FieldsString = ""
-
-    if ($Fields.Length > 0) {
-      $FieldsString = ("&sysparm_fields=" + ($Fields -join ","))
+    if ($Fields.Length -gt 0) {
+      $ReqUrl += ("&sysparm_fields=" + ($Fields -join ","))
     }
-
-    $resp = Invoke-WebRequest -Method Get -Credential $sncred "https://$snhost/api/now/table/$Table`?sysparm_query=$QueryText&sysparm_limit=$Limit$OffsetString$FieldsString"
-    
-    if (($resp.StatusCode - 200) -lt 100) {
-        $returnValue = ($resp.Content | ConvertFrom-Json).result
-    }
-
-    return $returnValue
   }
 
+  $resp = Invoke-WebRequest -Method Get -Credential $sncred "https://$snhost/api/now/table/$ReqUrl"
+    
+  if (($resp.StatusCode - 200) -lt 100) {
+    $returnValue = ($resp.Content | ConvertFrom-Json).result
+  }
+
+  return $returnValue
+}
+
 function Invoke-ServiceNowImageUpload($Connection, $Table, $SysId, $FilePath, $ImageField = "") {
-    $ContentTypeList = @{
-        ".jpg" = "image/jpeg"
-        ".jpeg" = "image/jpeg"
-        ".png" = "image/png"
-    }
+  $ContentTypeList = @{
+    ".jpg"  = "image/jpeg"
+    ".jpeg" = "image/jpeg"
+    ".png"  = "image/png"
+  }
 
-    $returnValue = $false
+  $returnValue = $false
 
-    $sncred = $Connection.creds
-    $snhost = $Connection.host
+  $sncred = $Connection.creds
+  $snhost = $Connection.host
     
-    $FileObj = (Get-Item $FilePath)
-    $FileType = $FileObj.Extension
-    $FileName = $FileObj.Name
+  $FileObj = (Get-Item $FilePath)
+  $FileType = $FileObj.Extension
+  $FileName = $FileObj.Name
 
-    $UploadData =  [IO.File]::ReadAllBytes($FilePath)
+  $UploadData = [IO.File]::ReadAllBytes($FilePath)
     
-    $UseTable = $ImageField.Length -eq 0 ? $Table : "ZZ_YY$Table"
+  $UseTable = $ImageField.Length -eq 0 ? $Table : "ZZ_YY$Table"
     
-    $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/octet-stream" } -Body $UploadData -Method Post -Credential $sncred "https://$snhost/api/now/attachment/file?table_name=$UseTable&table_sys_id=$SysId&file_name=$FileName")
+  $resp = (Invoke-WebRequest -Headers @{ "Accept" = "application/json" ; "Content-Type" = "application/octet-stream" } -Body $UploadData -Method Post -Credential $sncred "https://$snhost/api/now/attachment/file?table_name=$UseTable&table_sys_id=$SysId&file_name=$FileName")
 
-    if (($resp.StatusCode - 200) -lt 100) {
+  if (($resp.StatusCode - 200) -lt 100) {
         
     
-        if ($ImageField.Length -gt 0) {
-          $attachmentId = ($resp.Content | ConvertFrom-Json).result.sys_id
-          $UpdateData = (New-ServiceNowData)
-          $UpdateData.SetValue('file_name',$ImageField)
-          $UpdateData.SetValue('table_name',$UseTable)
-          $UpdateData.SetValue('content_type',$ContentTypeList[$FileType])
+    if ($ImageField.Length -gt 0) {
+      $attachmentId = ($resp.Content | ConvertFrom-Json).result.sys_id
+      $UpdateData = (New-ServiceNowData)
+      $UpdateData.SetValue('file_name', $ImageField)
+      $UpdateData.SetValue('table_name', $UseTable)
+      $UpdateData.SetValue('content_type', $ContentTypeList[$FileType])
 
-          $resp = (Invoke-ServiceNowUpdate -Connection $Connection -Table sys_attachment -SysId $attachmentId -Data $UpdateData)
+      $resp = (Invoke-ServiceNowUpdate -Connection $Connection -Table sys_attachment -SysId $attachmentId -Data $UpdateData)
 
-          $returnValue = ($resp.StatusCode - 200) -lt 100
-        }
-        else {
-          $returnValue = $true
-        }
+      $returnValue = ($resp.StatusCode - 200) -lt 100
     }
+    else {
+      $returnValue = $true
+    }
+  }
 
-    return $returnValue
+  return $returnValue
 }
